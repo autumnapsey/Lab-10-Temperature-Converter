24b0a3d14d33af753203d999dea69672ce48a8d4
Autumn Apsey <autumnapsey@gmail.com>
2016-04-18 14:07:50 -0400

    first commit

---
+++ tempfunction.js
@@ -0,0 +1,23 @@
+function convertTemp(number, unit){
+	if(unit === "F") {
+		var conversion = (number - 32)*5/9;
+		console.log(number + " degrees Farenheit converted to Celsius is " + conversion + ".");
+//		document.write(number + " degrees Farenheit converted to Celsius is " + conversion + ".") to print to HTML page
+	}
+		else if (unit === "C") {
+			var conversion = number * 9/5 + 32;
+			console.log(number + " degrees Celsius converted to Farenheit is " + conversion + ".");
+//			document.write(number + " degrees Celsius converted to Farenheit is " + conversion + ".") to print to HTML page
+		 }
+			else { 
+				console.log("Please enter unit of F or C and try again.");
+//				document.write("Please enter unit of F or C and try again.") to print to HTML page
+			}
+}
+
+var myNumber = prompt("What temperature would you like to convert?");
+var myUnit = prompt("What unit is your temperature? (F or C)");
+
+
+
+convertTemp(myNumber, myUnit);
