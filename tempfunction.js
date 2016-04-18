30e76a734d4aa797cd27ad51bd5d6b4989abc582
Autumn Apsey <autumnapsey@gmail.com>
2016-04-18 14:19:30 -0400

    fixed indentation and double variable declaration

--- tempfunction.js
+++ tempfunction.js
@@ -4,15 +4,15 @@ function convertTemp(number, unit){
 		console.log(number + " degrees Farenheit converted to Celsius is " + conversion + ".");
 //		document.write(number + " degrees Farenheit converted to Celsius is " + conversion + ".") to print to HTML page
 	}
-		else if (unit === "C") {
-			var conversion = number * 9/5 + 32;
-			console.log(number + " degrees Celsius converted to Farenheit is " + conversion + ".");
+	else if (unit === "C") {
+		conversion = number * 9/5 + 32;
+		console.log(number + " degrees Celsius converted to Farenheit is " + conversion + ".");
 //			document.write(number + " degrees Celsius converted to Farenheit is " + conversion + ".") to print to HTML page
-		 }
-			else { 
-				console.log("Please enter unit of F or C and try again.");
-//				document.write("Please enter unit of F or C and try again.") to print to HTML page
-			}
+	 }
+	else { 
+		console.log("Please enter unit of F or C and try again.");
+//		document.write("Please enter unit of F or C and try again.") to print to HTML page
+	}
 }
 
 var myNumber = prompt("What temperature would you like to convert?");
